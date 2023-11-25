feedf7922cd00d96be487e72e11f3a81dbbc9c14
rajat sinha <rajatsinha5467@gmail.com>
2023-11-25 21:08:46 +0530

    testing

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app=express()
 app.use(express.json())
 let PORT=process.env.PORT||8090
 app.use("/user",router)
+app.get("/",(req,res)=>{
+    res.send({msg:"hello world"})
+})
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
     connectDB()
