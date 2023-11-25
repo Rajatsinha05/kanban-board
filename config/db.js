55f0fa8bd1d70d05d248bcd72907c4f2311938a8
rajat sinha <rajatsinha5467@gmail.com>
2023-11-26 01:14:24 +0530

    test

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const connectDB = async () => {
   let url = process.env.DB_URL;
   try {
-    await mongoose.connect(url);
+    await mongoose.connect("mongodb+srv://daksh1or2:kanban@cluster0.bcmpzhn.mongodb.net/");
     console.log("Connected to the database");
   } catch (error) {
     console.error("Error connecting to the database:", error.message);
