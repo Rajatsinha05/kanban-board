73cd6dbde0f882f0a5e3de37fae42246907a8a0d
rajat sinha <rajatsinha5467@gmail.com>
2023-11-28 02:51:19 +0530

    cors bug

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const task = require("./routes/task.route")
 const app=express()
 app.use(cors())
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT,PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
 app.use(express.json())
